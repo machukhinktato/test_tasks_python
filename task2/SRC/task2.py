9801e5d459c6c5684c55c211984cb93243ae87ec
machukhinktato <tarabrinmv@gmail.com>
2020-11-11 14:14:12 +0300

    in progress

--- task2/SRC/task2.py
+++ task2/SRC/task2.py
@@ -20,13 +20,27 @@ def sphere_theme():
                (z + task_dict.get('sphere').get('radius')))))
     # x, y, z = [[i for i in range(-10, 10)] for i in range(3)]
 
-    line_x = list(((task_dict.get('line')[0][0]),(task_dict.get('line')[1][0])))
-    line_y = list(((task_dict.get('line')[0][1]),(task_dict.get('line')[1][1])))
-    line_z = list(((task_dict.get('line')[0][2]),(task_dict.get('line')[1][2])))
+    line_x = list(((task_dict.get('line')[0][0]),
+                   (task_dict.get('line')[1][0])))
+    line_y = list(((task_dict.get('line')[0][1]),
+                   (task_dict.get('line')[1][1])))
+    line_z = list(((task_dict.get('line')[0][2]),
+                   (task_dict.get('line')[1][2])))
 
+
+    def comparison():
+        pass
+
+
+    if line_x[0] < sphere_x[0]:
+        print('banana')
+    elif line_x[0] >= sphere_x[0] and line_x[0] >= sphere_x[1]:
+        print('zhopa')
+    elif line_x[0] >= sphere_x[0] and line_x[0] < sphere_x[1]:
+        print('blyad')
     # return print(task_dict.get('line')[1][1])
     # return print(sphere_x, sphere_y, sphere_z)
-    return print(line_x, line_y, line_z)
-
+    # return print(line_x, line_y, line_z)
+    return print(line_x, sphere_x)
 if __name__ == '__main__':
     sphere_theme()
