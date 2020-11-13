f16589bc27cacc9241e49bd90af55ff8d952164d
machukhinktato <tarabrinmv@gmail.com>
2020-11-13 16:18:23 +0300

    in progress

--- task3/SRC/task3.py
+++ task3/SRC/task3.py
@@ -1,6 +1,10 @@
 import csv
 import re
 
+log = 'log.log'
+start = '2020-01-01Т12:51:33'
+end = '2020-01-01Т12:51:34'
+
 
 def file_loader(data):
     with open(data, 'r', encoding='utf8') as f:
@@ -22,32 +26,54 @@ def save_data(data=None):
     return 'done'
 
 
-def log_scraper():
-    data = file_loader('log.log')
+def log_scraper(log, start, end):
+    data = file_loader(log)
     data = [i.split(',') for i in data]
     save_data(data)
     # barrel_data = dict()
     barrel_max_cap = re.findall('[0-9]+', data[0][0]).pop()
     barrel_cap_at_start = re.findall('[0-9]+', data[1][0]).pop()
-    log_data, log_action = [], []
+    # print(data)
+    log_data, success, fail = [], [], []
     for i in range(len(data)):
         try:
-            log_data.append(data[i + 2][0].split('.')[0])
-            log_action.append(
-                data[i + 2][0].split('.')[1].split('-')[1].
-                    replace('\n', ''))
+            if start > data[i + 2][0].split('.')[0]:
+                log_data.append(data[i + 2])
         except:
             continue
+    for elem in log_data:
+        for i in range(len(log_data)):
+            if elem[-i].split('.')[0] > end:
+                log_data.pop([-i])
 
-    # barrel_data[w_vol] = re.match('[0-9]+', data[0][0])
-    # for i in range(len(data)):
-    #     data[i][0].replace('\n', '')
-    #     print(data[i][0])
-    # print([data[i] for i in range(len(data))][0])
-    return print(log_data, log_action)
-    # return print(data[3][0].split('.')[1].split('-')[1])
+            # log_data.append(data[i + 2][0].split('.')[0])
+    #                             if 'успех' in data[i + 2][0].split('.')[1].split('-')[1]:
+    #                                 success.append(
+    #                                     data[i + 2][0].split('.')[1].split('-')[1].
+    #                                         replace('\n', ''))
+    #                             else:
+    #                                 fail.append(
+    #                                     data[i + 2][0].split('.')[1].split('-')[1].
+    #                                         replace('\n', ''))
+
+
+    # err_percentage = len(fail) * 100 / (len(success) + len(fail))
+    # for i in range(len(log_data)):
+    #     if start < log_data[i]:
+    #         # log_data[i].pop()
+    #         log_data.pop(i)
+
+
+# barrel_data[w_vol] = re.match('[0-9]+', data[0][0])
+# for i in range(len(data)):
+#     data[i][0].replace('\n', '')
+#     print(data[i][0])
+# print([data[i] for i in range(len(data))][0])
+# return print(start, log_data, end)
+    return print(log_data)
+# return print(data[3][0].split('.')[1].split('-')[1])
 
 
 if __name__ == '__main__':
-    log_scraper()
+    log_scraper(log, start, end)
     # save_data()
