76f6a627d118217451b684034cc2c3e81d8e2817
machukhinktato <tarabrinmv@gmail.com>
2020-11-16 10:28:04 +0300

    done

--- task4/SRC/task4.py
+++ task4/SRC/task4.py
@@ -1,11 +1,17 @@
-import re
-
-
-def main(str1, str2):
-    coincidence = False
-
-
-    return ['OK' if coincidence else 'KO']
+def main(str1=None, str2=None):
+    if str1 == None or str2 == None:
+        return print('В функцию не были переданы аргументы.\n'
+                     'Принимаются аргументы строчного типа.\n'
+                     'Процесс будет завершен.')
+    is_same = False
+    length = len(str2[0:(str2.index('*'))])
+    if '*' in str2:
+        is_same = True if str1[0:length] == str2[0:length] else False
+    elif str1 == str2:
+        is_same = True
+    elif "*" == str2[0]:
+        is_same = True
+    return print(['OK' if is_same else 'KO'][0])
 
 
 if __name__ == '__main__':
